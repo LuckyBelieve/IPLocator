cdd80d21a8887968144ed3b497b83c9b2684f4c8
LuckyBelieve <luckybelieve10@gmail.com>
2024-01-15 20:34:36 +0200

    custom hook for fetching the location

--- src/App.jsx
+++ src/App.jsx
@@ -1,44 +1,46 @@
-import { useState } from "react"
-import SearchInput from "./components/SearchInput"
+import { useState } from "react";
+import SearchInput from "./components/SearchInput";
 import LocationDetails from "./components/LocationDetails";
 import Map from "./components/Map";
 import apiClient from "./services/apiClient";
+import useFetchLocation from "./hooks/useFetchLocation";
 
 function App() {
-  const [ipAddress,setIpAddress] = useState(0);
 
+  const { data, isLoading, error } = useFetchLocation(
+    "/country,city",
+    0
+  );
+
+  const [ipAddress, setIpAddress] = useState(0);
+  const [fetchedLocation,setFetchedLocation] = useState(data);
+  
   // handling ip address from the search input
-  const handleIpSearch = (ipAddress)=>{
+  const handleIpSearch = (ipAddress) => {
     setIpAddress(ipAddress);
   };
 
-
-  const searchIpLocation = ()=>{
-    if(ipAddress){
-      const data = apiClient.get("/country,city",{
-      params:{
-        ipAddress
-      }
-    })
-
-    console.log(data);
-    }else return;
+  const searchIpLocation = () => {
     
-  }
-
+  };
 
   return (
     <div>
       <div className=" z-30 relative flex flex-col items-center justify-center w-full bg-[url('/bgdesk.png')] bg-cover pb-32">
-        <h1 className="text-3xl font-sans font-semibold py-5 text-white">IP Address Tracker</h1>
-      <SearchInput handleIpSearch={handleIpSearch} search ={searchIpLocation}/>
-      <LocationDetails/>
+        <h1 className="text-3xl font-sans font-semibold py-5 text-white">
+          IP Address Tracker
+        </h1>
+        <SearchInput
+          handleIpSearch={handleIpSearch}
+          search={searchIpLocation}
+        />
+        <LocationDetails Location={data} />
       </div>
       <div>
-        <Map/>
+        <Map />
       </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
